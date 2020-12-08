326cbb3285b7dc6690cd38e113275d0449bcfa8c
weizujie <byojiaoxianz7@outlook.com>
2020-12-08 13:15:41 +0800

    完成角色授权功能

---
+++ SpringBoot-Shiro/sql/springboot-shiro.sql
@@ -0,0 +1,119 @@
+/*
+SQLyog Community v12.4.0 (64 bit)
+MySQL - 8.0.22 : Database - springboot-shiro
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`springboot-shiro` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `springboot-shiro`;
+
+/*Table structure for table `t_perms` */
+
+DROP TABLE IF EXISTS `t_perms`;
+
+CREATE TABLE `t_perms` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  `mark` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_perms` */
+
+insert  into `t_perms`(`id`,`name`,`mark`) values 
+(1,'sys:user:add','用户添加'),
+(2,'sys:user:delete','用户删除'),
+(3,'sys:user:update','用户更新'),
+(4,'sys:user:list','用户列表');
+
+/*Table structure for table `t_role` */
+
+DROP TABLE IF EXISTS `t_role`;
+
+CREATE TABLE `t_role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `mark` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_role` */
+
+insert  into `t_role`(`id`,`name`,`mark`) values 
+(1,'admin','管理员'),
+(2,'teacher','教师'),
+(3,'student','学生');
+
+/*Table structure for table `t_role_perms` */
+
+DROP TABLE IF EXISTS `t_role_perms`;
+
+CREATE TABLE `t_role_perms` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role_id` int DEFAULT NULL,
+  `perms_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_role_perms` */
+
+insert  into `t_role_perms`(`id`,`role_id`,`perms_id`) values 
+(1,1,1),
+(2,1,2),
+(3,1,3),
+(4,1,4),
+(5,2,1),
+(6,2,2),
+(7,2,3),
+(8,2,4);
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `salt` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user` */
+
+insert  into `t_user`(`id`,`username`,`password`,`salt`) values 
+(1,'admin','6854b5437e3dd84e925bdcacf0b5020c','Fif2ZN3j'),
+(2,'teacher','607b87a3cd44aaf0f80b0964d0ac4144','Ax7eMhlU'),
+(3,'student','729a573fa5c4f685277b84501e3c667d','zOA5OGA7');
+
+/*Table structure for table `t_user_role` */
+
+DROP TABLE IF EXISTS `t_user_role`;
+
+CREATE TABLE `t_user_role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user_role` */
+
+insert  into `t_user_role`(`id`,`user_id`,`role_id`) values 
+(1,1,1),
+(2,2,2),
+(3,3,3);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
